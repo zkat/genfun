bfe49c3ebd4411019a2a6b9a6aa126d070f9d9d0
Josh Marchán <sykopomp@sykosomatic.org>
2013-02-27 01:07:18 -0500

    Nicer test suite

--- test.js
+++ test.js
@@ -8,46 +8,55 @@
         frobnicate.addMethod(
             [Number.prototype],
             function(num) {
-                console.log("One number: ", num);
+                return "one number: "+num;
             });
 
         frobnicate.addMethod(
             [String.prototype],
             function(str) {
-                console.log("One string: ", str);
+                return "one string: "+str;
             });
 
         frobnicate.addMethod(
             [String.prototype, Number.prototype],
             function(string, number) {
-                console.log("Got a string and a number: ", string, number);
+                return "string + number: " + string + ", " + number;
             });
 
         frobnicate.addMethod(
             [Number.prototype, String.prototype],
             function(number, string) {
-                console.log("Got a number and a string: ", number, string);
+                return "number + string: " + number + ", " + string;
             });
 
         frobnicate.addMethod(
             [],
             function() {
-                console.log("Dispatch fell through to the default method: ", arguments);
+                return "Dispatch fell through";
             });
 
         frobnicate.addMethod(
             [Number.prototype, , String.prototype],
-            function(num, anything, string) {
-                console.log("A number, anything, and then a string.");
+            function(number, anything, string) {
+                return "number + anything + string: " + number + ", " + anything + ", " + string;
             });
 
-        frobnicate(new String("foo"), new Number(1)); // Got a string and a number
-        frobnicate(1, "foo"); // Got a number and a string
-        frobnicate(1, 1); // One number
-        frobnicate(1); // One number
-        frobnicate("str"); // One string
-        frobnicate(true); // Dispatch fell through
-        frobnicate(); // Dispatch fell through
-        frobnicate(1, true, "foo"); // A number, anything, and then a string.
+        test("string + number: foo, 1", [new String("foo"), new Number(1)]);
+        test("number + string: 1, foo", [1, "foo"]);
+        test("one number: 1", [1, 2]);
+        test("one number: 1", [1]);
+        test("one string: str", ["str"]);
+        test("Dispatch fell through", [true]);
+        test("Dispatch fell through", []);
+        test("number + anything + string: 1, true, foo", [1, true, "foo"]);
+
+        console.log("Tests passed!");
+
+        function test(expected, args) {
+            var result = frobnicate.apply(this, args);
+            if (result != expected) {
+                throw Error("Tests failed. Expected "+expected+" but got "+result);
+            }
+        };
     };
 })(Genfun);
