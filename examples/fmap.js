71e84bba38d1a2be8df0c312cabfc85f07605454
Josh Marchán <sykopomp@sykosomatic.org>
2013-08-21 23:57:27 -0700

    Untabifying examples/fmap.js

--- examples/fmap.js
+++ examples/fmap.js
@@ -1,6 +1,6 @@
 /* -*- js2-basic-offset: 2; indent-tabs-mode: nil; -*- */
 var Genfun = require("../build/genfun"),
-	addMethod = Genfun.addMethod;
+    addMethod = Genfun.addMethod;
 
 var fmap = new Genfun;
 
@@ -8,71 +8,71 @@ var fmap = new Genfun;
  * Maybe
  */
 function Just(value) {
-	this.value = value;
+  this.value = value;
 }
 var Nothing = {};
 
 addMethod(fmap, [Function.prototype, Just.prototype], function(f, just) {
-	return new Just(f.call(this, just.value));
+  return new Just(f.call(this, just.value));
 });
 addMethod(fmap, [Function.prototype, Nothing], function() {
-	return Nothing;
+  return Nothing;
 });
 
 /*
  * Array
  */
 addMethod(fmap, [Function.prototype, Array.prototype], function(f, array) {
-	return array.map(f, this);
+  return array.map(f, this);
 });
 
 /*
  * Tree
  */
 function Leaf(value) {
-	this.value = value;
+  this.value = value;
 }
 function Branch(left, right) {
-	this.left = left;
-	this.right = right;
+  this.left = left;
+  this.right = right;
 }
 
 addMethod(fmap, [Function.prototype, Leaf.prototype], function(f, leaf) {
-	return new Leaf(f.call(this, leaf.value));
+  return new Leaf(f.call(this, leaf.value));
 });
 addMethod(fmap, [Function.prototype, Branch.prototype], function(f, branch) {
-	return new Branch(f.call(this, branch.left), f.call(this, branch.right));
+  return new Branch(f.call(this, branch.left), f.call(this, branch.right));
 });
 
 function runExample() {
-	function logValue(type) {
-		return function(val) {
-			console.log("Mapping over ", val, " in object of type "+type);
-		};
-	}
+  function logValue(type) {
+    return function(val) {
+      console.log("Mapping over ", val, " in object of type "+type);
+    };
+  }
 
-	console.log("Testing Maybe");
-	fmap(logValue("Just"), new Just(123));
-	fmap(logValue("Nothing"), Nothing);
-	
-	console.log("Testing Array");
-	fmap(logValue("Array"), [1,2,3]);
+  console.log("Testing Maybe");
+  fmap(logValue("Just"), new Just(123));
+  fmap(logValue("Nothing"), Nothing);
+  
+  console.log("Testing Array");
+  fmap(logValue("Array"), [1,2,3]);
 
-	console.log("Testing Tree");
-	function br(a, b) { return new Branch(a, b); }
-	function lf(x) { return new Leaf(x); }
-	fmap(logValue("Tree"),
-		 br(
-			 br(
-				 lf(1),
-				 lf(2)),
-			 br(
-				 br(
-					 lf(3),
-					 br(
-						 lf(4),
-						 lf(5))),
-				 lf(6))));
+  console.log("Testing Tree");
+  function br(a, b) { return new Branch(a, b); }
+  function lf(x) { return new Leaf(x); }
+  fmap(logValue("Tree"),
+       br(
+         br(
+           lf(1),
+           lf(2)),
+         br(
+           br(
+             lf(3),
+             br(
+               lf(4),
+               lf(5))),
+           lf(6))));
 }
 
 exports.runExample = runExample;
