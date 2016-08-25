dd3d497c45e5dfa9c8b2d254a1d14df8ee8b0a44
Kat Marchán <kzm@sykosomatic.org>
2016-08-24 20:19:21 -0700

    make benchmark node-runnable

--- bench.js
+++ bench.js
@@ -1,68 +1,59 @@
-;(function(Genfun) {
+var genfun = require('./')
 
-    Genfun.arewefastyet = function() {
-        /*
-         * Test setup
-         */
-        function TestObj() {};
-        var test_obj = new TestObj();
-        var test_genfun = new Genfun();
+/*
+* Test setup
+*/
+function TestObj () {}
+var test_obj = new TestObj()
+var test_genfun = genfun()
 
-        /*
-         * Tests
-         */
+/*
+* Tests
+*/
 
-        function test_function(test_obj) {
-            return test_obj.toString() + "test";
-        };
-        bench("Regular function", function() {
-            test_function(test_obj);
-        });
+function test_function (test_obj) {
+  return test_obj.toString() + 'test'
+}
+bench('Regular function', function () {
+  test_function(test_obj)
+})
 
-        TestObj.prototype.test_method = function() {
-            return this.toString() + "test";
-        };
-        bench("Native method", function() {
-            test_obj.test_method();
-        });
+TestObj.prototype.test_method = function () {
+  return this.toString() + 'test'
+}
+bench('Native method', function () {
+  test_obj.test_method()
+})
 
-        test_genfun.addMethod(
-            [TestObj.prototype],
-            function(test_obj) {
-                return test_obj.toString() + "test";
-            });
-        bench("Singly-dispatched genfun", function() {
-            test_genfun(test_obj);
-        });
+test_genfun.add([TestObj.prototype], function (test_obj) {
+  return test_obj.toString() + 'test'
+})
+bench('Singly-dispatched genfun', function () {
+  test_genfun(test_obj)
+})
 
-        test_genfun.addMethod(
-            [TestObj.prototype, TestObj.prototype],
-            function(test_obj1, test_obj2) {
-                return test_obj.toString() + "test";
-            });
-        bench("Double-dispatched genfun", function() {
-            test_genfun(test_obj, test_obj);
-        });
+test_genfun.add([TestObj.prototype, TestObj.prototype], function (test_obj1, test_obj2) {
+  return test_obj.toString() + 'test'
+})
+bench('Double-dispatched genfun', function () {
+  test_genfun(test_obj, test_obj)
+})
 
-        test_genfun.addMethod(
-            [TestObj.prototype, String.prototype],
-            function(test_obj1, string) {
-                return test_obj.toString() + string;
-            });
-        bench("Double-dispatched genfun with string primitive", function() {
-            test_genfun(test_obj, "test");
-        });
+test_genfun.add([TestObj.prototype, String.prototype], function (test_obj1, string) {
+  return test_obj.toString() + string
+})
+bench('Double-dispatched genfun with string primitive', function () {
+  test_genfun(test_obj, 'test')
+})
 
-        /*
-         * Util
-         */
-        function bench(name, func, iterations) {
-            iterations = iterations || 100000;
-            console.time(name);
-            for (var i = 0; i < iterations; i++) {
-                func();
-            }
-            console.timeEnd(name);
-        }
-    };
-})(Genfun);
+/*
+* Util
+*/
+function bench (name, func, iterations) {
+  iterations = iterations || 100000
+  console.time(name)
+  for (var i = 0; i < iterations; i++) {
+    func()
+  }
+  console.timeEnd(name)
+}
